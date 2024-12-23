7d072a9a675ee0ec9996c6099b369257d60ae852
EvanMcCormick37 <87681272+EvanMcCormick37@users.noreply.github.com>
2024-12-23 01:51:07 -0500

    Added Paper to docs
    -created Docs folder, added one page with finished paper as index.html
    -Re-trained models on SCG, ECG, and SECG
    -Recreated graphs for SCG, ECG, and SECG using modified graphing functions.
    -Deleted 'models' folder as it is redundant, now 'best_model_save_path' is used within the /log folder.

--- functions.py
+++ functions.py
@@ -21,51 +21,55 @@ def moving_average(values, window, gaussian = False):
         x = np.arange(-(window // 2), window // 2 + 1)
         kernel = np.exp(-(x ** 2) / (2 * window ** 2))
         kernel = kernel / np.sum(kernel)
+    
     #The convolve function iteratively multiplies the first n values in the values array by the weights array.
     # with the given weights array, it essentially takes the moving average of each N values in the values array.
     return np.convolve(values, kernel, "valid")
 
-def plot_results(log_folder, start = 0, stop = None, window = 1, gaussian = False, title="Learning Curve"):
+def plot_results(log_folder, x_range = None, smoothing = True, window = 100, gaussian = True, title="Learning Curve"):
     """
     plot the results
 
     :param log_folder: (str) the save location of the resFults to plot
+    :param x_range: (str) the range of the plot on the X-axis
+    :param smoothing: (bool) whether to use smoothing on the learning curve
+    :param window: (int) the size of the smoothing kernel
+    :param gaussian: (bool) whether to use a gaussian or flat smoothing convolution kernel
     :param title: (str) the title of the task to plot
-    :param gaussian: (bool) whether to use a flat or gaussian convolution kernel
     """
 
     x, y = ts2xy(load_results(log_folder), "timesteps")
-    y = moving_average(y, window = window, gaussian = gaussian)
-    
-    # Truncate x
-    if stop is None:
-        stop = len(x)
-    x = x[(len(x) - len(y)) + start : stop]
-    
+    if smoothing is True:
+        y = moving_average(y, window = window, gaussian = gaussian)
+    x = x[(len(x) - len(y)):]
     # Plot
-    plt.plot(x,y)
+    plt.plot(np.arange(len(y)),y)
     plt.xlabel("# of Training Timesteps")
     plt.ylabel("Mean Reward")
     plt.title(title + f"Smoothed, window-size = {window}")
     plt.show()
 
-def plot_multi(log_folders, labels, start = 0, stop = None, window = 1, gaussian = False, title="Learning Curve "):
+def plot_multi(log_folders, labels, x_range = None, smoothing = True, window = 100, gaussian = False, title="Learning Curve "):
     """
     plot the results
 
-    :param log_folder: (str) the save location of the results to plot
+    :param log_folders: (str) the save locations of the results for each line in the multi-line plot
+    :param labels: The labels of each line in the multi-line plot.
+    :param x_range: (str) the range of the plot on the X-axis
+    :param smoothing: (bool) whether to use smoothing on the learning curve
+    :param window: (int) the size of the smoothing kernel
+    :param gaussian: (bool) whether to use a gaussian or flat smoothing convolution kernel
     :param title: (str) the title of the task to plot
     """
     for i, log_folder in enumerate(log_folders):
         x, y = ts2xy(load_results(log_folder), "timesteps")
-        y = moving_average(y, window = window, gaussian = gaussian)
-        
+        if smoothing is True:
+            y = moving_average(y, window = window, gaussian = gaussian)
         # Truncate x
-        if stop is None:
-            stop = len(x)
-        x = x[(len(x) - len(y)) + start : stop if stop < len(x) else len(x)]
+        x = x[(len(x) - len(y)):]
         plt.plot(x,y, label = labels[i])
-
+    # if x_range is not None:
+    #     plt.xlim(x_range[0],x_range[1])
     plt.xlabel("# of Training Timesteps")
     plt.ylabel("Mean Reward")
     plt.legend()
